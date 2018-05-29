03f38dc237841f45c24c183e76c7601807fcfb16
yihanseattle <yihanseattle@gmail.com>
2018-05-29 12:46:02 -0700

    Part 7: Add orientation.

--- app/src/main/java/com/rokid/camera/camera2videoimage/Camera2VideoImageActivity.java
+++ app/src/main/java/com/rokid/camera/camera2videoimage/Camera2VideoImageActivity.java
@@ -11,6 +11,8 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseIntArray;
+import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 
@@ -62,6 +64,13 @@ public class Camera2VideoImageActivity extends AppCompatActivity {
     private String mCameraId;
     private HandlerThread mBackgroundHandlerThread;
     private Handler mBackgroundHandler;
+    private static SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 0);
+        ORIENTATIONS.append(Surface.ROTATION_90, 90);
+        ORIENTATIONS.append(Surface.ROTATION_180, 180);
+        ORIENTATIONS.append(Surface.ROTATION_270, 270);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,6 +124,16 @@ public class Camera2VideoImageActivity extends AppCompatActivity {
                 if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
                     continue;
                 }
+                int deviceOrientation = getWindowManager().getDefaultDisplay().getRotation();
+                int totalRotation = sensorToDeviceRotation(cameraCharacteristics, deviceOrientation);
+                boolean swapRotation = totalRotation == 90 || totalRotation == 270;
+                int rotatedWidth = width;
+                int rotatedHeight = height;
+                if (swapRotation) {
+                    // in portrait mode, flip the orientation
+                    rotatedWidth = height;
+                    rotatedHeight = width;
+                }
                 mCameraId = cameraId;
                 return;
             }
@@ -145,5 +164,11 @@ public class Camera2VideoImageActivity extends AppCompatActivity {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    private static int sensorToDeviceRotation(CameraCharacteristics cameraCharacteristics, int deviceOrientation) {
+        int sensorOrientation = cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
+        deviceOrientation = ORIENTATIONS.get(deviceOrientation);
+        return (sensorOrientation + deviceOrientation + 360) % 360;
     }
 }
