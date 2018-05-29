7ff9d91f8616f192131af5eacce7cd7daf82cdc5
yihanseattle <yihanseattle@gmail.com>
2018-05-29 10:37:28 -0700

    Part 2: Make app full screen.

--- app/src/main/java/com/rokid/camera/camera2videoimage/Camera2VideoImageActivity.java
+++ app/src/main/java/com/rokid/camera/camera2videoimage/Camera2VideoImageActivity.java
@@ -1,7 +1,8 @@
 package com.rokid.camera.camera2videoimage;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Camera2VideoImageActivity extends AppCompatActivity {
 
@@ -9,5 +10,19 @@ public class Camera2VideoImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera2_video_image);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
     }
 }
