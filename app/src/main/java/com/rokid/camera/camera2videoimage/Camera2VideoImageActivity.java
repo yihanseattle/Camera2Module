c3f2c9978efacd264450e20ff21ce1c56399d2ed
yihanseattle <yihanseattle@gmail.com>
2018-05-31 23:33:23 -0700

    Working autofittextureview.

--- app/src/main/java/com/rokid/camera/camera2videoimage/Camera2VideoImageActivity.java
+++ app/src/main/java/com/rokid/camera/camera2videoimage/Camera2VideoImageActivity.java
@@ -28,7 +28,6 @@ import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.util.Size;
 import android.util.SparseIntArray;
 import android.view.Surface;
@@ -668,59 +667,75 @@ public class Camera2VideoImageActivity extends AppCompatActivity {
         return (sensorOrientation + deviceOrientation + 360) % 360;
     }
 
-    /**
-     * Given {@code choices} of {@code Size}s supported by a camera, choose the smallest one that
-     * is at least as large as the respective texture view size, and that is at most as large as the
-     * respective max size, and whose aspect ratio matches with the specified value. If such size
-     * doesn't exist, choose the largest one that is at most as large as the respective max size,
-     * and whose aspect ratio matches with the specified value.
-     *
-     * @param choices           The list of sizes that the camera supports for the intended output
-     *                          class
-     * @param textureViewWidth  The width of the texture view relative to sensor coordinate
-     * @param textureViewHeight The height of the texture view relative to sensor coordinate
-     * @param maxWidth          The maximum width that can be chosen
-     * @param maxHeight         The maximum height that can be chosen
-     * @param aspectRatio       The aspect ratio
-     * @return The optimal {@code Size}, or an arbitrary one if none were big enough
-     */
-    private static Size chooseOptimalSize(Size[] choices, int textureViewWidth,
-                                          int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio) {
+//    /**
+//     * Given {@code choices} of {@code Size}s supported by a camera, choose the smallest one that
+//     * is at least as large as the respective texture view size, and that is at most as large as the
+//     * respective max size, and whose aspect ratio matches with the specified value. If such size
+//     * doesn't exist, choose the largest one that is at most as large as the respective max size,
+//     * and whose aspect ratio matches with the specified value.
+//     *
+//     * @param choices           The list of sizes that the camera supports for the intended output
+//     *                          class
+//     * @param textureViewWidth  The width of the texture view relative to sensor coordinate
+//     * @param textureViewHeight The height of the texture view relative to sensor coordinate
+//     * @param maxWidth          The maximum width that can be chosen
+//     * @param maxHeight         The maximum height that can be chosen
+//     * @param aspectRatio       The aspect ratio
+//     * @return The optimal {@code Size}, or an arbitrary one if none were big enough
+//     */
+//    private static Size chooseOptimalSize(Size[] choices, int textureViewWidth,
+//                                          int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio) {
+//
+//        //Log.d(TAG, "chooseOptimalSize: tVW = " + textureViewWidth + " tVH = " + textureViewHeight);
+//        //Log.d(TAG, "chooseOptimalSize: maxW = " + maxWidth + " maxH = " + maxHeight);
+//        // Collect the supported resolutions that are at least as big as the preview Surface
+//        List<Size> bigEnough = new ArrayList<>();
+//        // Collect the supported resolutions that are smaller than the preview Surface
+//        List<Size> notBigEnough = new ArrayList<>();
+//        int w = aspectRatio.getWidth();
+//        int h = aspectRatio.getHeight();
+//        for (Size option : choices) {
+//            //Log.d(TAG, "chooseOptimalSize: option = " + option.getWidth() + " " + option.getHeight());
+//            if (option.getWidth() <= maxWidth && option.getHeight() <= maxHeight) {// &&
+//                //    option.getHeight() == option.getWidth() * h / w) {
+//                if (option.getHeight() != option.getWidth() * h / w) continue;
+//                if (option.getWidth() >= textureViewWidth &&
+//                        option.getHeight() >= textureViewHeight) {
+//                    bigEnough.add(option);
+//                } else {
+//                    notBigEnough.add(option);
+//                }
+//            }
+//        }
+//
+//        // Pick the smallest of those big enough. If there is no one big enough, pick the
+//        // largest of those not big enough.
+//        if (bigEnough.size() > 0) {
+//            //Log.d(TAG, "chooseOptimalSize: Goes to bigEnough");
+//            return Collections.min(bigEnough, new CompareSizeByArea());
+//        } else if (notBigEnough.size() > 0) {
+//            //Log.d(TAG, "chooseOptimalSize: Goes to notBigEnough");
+//            return Collections.max(notBigEnough, new CompareSizeByArea());
+//        } else {
+//            //Log.d(TAG, "chooseOptimalSize: Not find any suitable!");
+//            Log.e(TAG, "Couldn't find any suitable preview size");
+//            return choices[0];
+//        }
+//    }
 
-        //Log.d(TAG, "chooseOptimalSize: tVW = " + textureViewWidth + " tVH = " + textureViewHeight);
-        //Log.d(TAG, "chooseOptimalSize: maxW = " + maxWidth + " maxH = " + maxHeight);
-        // Collect the supported resolutions that are at least as big as the preview Surface
+    private static Size chooseOptimalSize(Size[] choices, int width, int height) {
         List<Size> bigEnough = new ArrayList<>();
-        // Collect the supported resolutions that are smaller than the preview Surface
-        List<Size> notBigEnough = new ArrayList<>();
-        int w = aspectRatio.getWidth();
-        int h = aspectRatio.getHeight();
         for (Size option : choices) {
-            //Log.d(TAG, "chooseOptimalSize: option = " + option.getWidth() + " " + option.getHeight());
-            if (option.getWidth() <= maxWidth && option.getHeight() <= maxHeight) {// &&
-                //    option.getHeight() == option.getWidth() * h / w) {
-                if (option.getHeight() != option.getWidth() * h / w) continue;
-                if (option.getWidth() >= textureViewWidth &&
-                        option.getHeight() >= textureViewHeight) {
-                    bigEnough.add(option);
-                } else {
-                    notBigEnough.add(option);
-                }
+            if (option.getHeight() == option.getWidth() * height / width &&
+                    option.getWidth() >= width && option.getHeight() >= height) {
+                bigEnough.add(option);
             }
         }
 
-        // Pick the smallest of those big enough. If there is no one big enough, pick the
-        // largest of those not big enough.
         if (bigEnough.size() > 0) {
-            //Log.d(TAG, "chooseOptimalSize: Goes to bigEnough");
             return Collections.min(bigEnough, new CompareSizeByArea());
-        } else if (notBigEnough.size() > 0) {
-            //Log.d(TAG, "chooseOptimalSize: Goes to notBigEnough");
-            return Collections.max(notBigEnough, new CompareSizeByArea());
         } else {
-            //Log.d(TAG, "chooseOptimalSize: Not find any suitable!");
-            Log.e(TAG, "Couldn't find any suitable preview size");
-            return choices[0];
+            return choices[2];
         }
     }
 
