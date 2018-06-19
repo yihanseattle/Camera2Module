50f722cf895f3f454c5b4d066e935c4f32ff30cd
yihanseattle <yihanseattle@gmail.com>
2018-06-18 19:44:37 -0700

    Fix imagesize bug.

--- app/src/main/java/com/rokid/glass/camera/MainActivity.java
+++ app/src/main/java/com/rokid/glass/camera/MainActivity.java
@@ -67,11 +67,8 @@ import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -872,45 +869,53 @@ public class MainActivity extends AppCompatActivity {
     private static Size chooseOptimalSize(Size[] choices, int textureViewWidth,
                                           int textureViewHeight, int maxWidth, int maxHeight, Size aspectRatio) {
 
-        // Collect the supported resolutions that are at least as big as the preview Surface
-        List<Size> bigEnough = new ArrayList<>();
-        // Collect the supported resolutions that are smaller than the preview Surface
-        List<Size> notBigEnough = new ArrayList<>();
+//        // Collect the supported resolutions that are at least as big as the preview Surface
+//        List<Size> bigEnough = new ArrayList<>();
+//        // Collect the supported resolutions that are smaller than the preview Surface
+//        List<Size> notBigEnough = new ArrayList<>();
+//
+//        List<Double> ratio = new LinkedList<>();
+//
+//        int w = aspectRatio.getWidth();
+//        int h = aspectRatio.getHeight();
+//        for (Size option : choices) {
+//            if (option.getWidth() > option.getHeight()) {
+//                ratio.add((double)option.getWidth() / option.getHeight());
+//            } else {
+//                ratio.add((double)option.getHeight() / option.getWidth());
+//            }
+//            if (option.getWidth() <= maxWidth && option.getHeight() <= maxHeight &&
+//                    option.getHeight() == option.getWidth() * h / w) {
+//                if (option.getWidth() >= textureViewWidth &&
+//                        option.getHeight() >= textureViewHeight) {
+//                    bigEnough.add(option);
+//                } else {
+//                    notBigEnough.add(option);
+//                }
+//            }
+//        }
+//
+//        // Pick the smallest of those big enough. If there is no one big enough, pick the
+//        // largest of those not big enough.
+//        if (bigEnough.size() > 0) {
+//            return Collections.min(bigEnough, new CompareSizeByArea());
+//        } else if (notBigEnough.size() > 0) {
+//            return Collections.max(notBigEnough, new CompareSizeByArea());
+//        } else {
+//            Log.e(TAG, "Couldn't find any suitable preview size");
+//            if (DeviceConfig.isInRokidGlass) {
+//                return choices[5];
+//            } else {
+//                return choices[choices.length - 1];
+//            }
+//        }
 
-        List<Double> ratio = new LinkedList<>();
+        // TODO: temporary fix because the preview size in xml has been set to [1dp x 1dp]
 
-        int w = aspectRatio.getWidth();
-        int h = aspectRatio.getHeight();
-        for (Size option : choices) {
-            if (option.getWidth() > option.getHeight()) {
-                ratio.add((double)option.getWidth() / option.getHeight());
-            } else {
-                ratio.add((double)option.getHeight() / option.getWidth());
-            }
-            if (option.getWidth() <= maxWidth && option.getHeight() <= maxHeight &&
-                    option.getHeight() == option.getWidth() * h / w) {
-                if (option.getWidth() >= textureViewWidth &&
-                        option.getHeight() >= textureViewHeight) {
-                    bigEnough.add(option);
-                } else {
-                    notBigEnough.add(option);
-                }
-            }
-        }
-
-        // Pick the smallest of those big enough. If there is no one big enough, pick the
-        // largest of those not big enough.
-        if (bigEnough.size() > 0) {
-            return Collections.min(bigEnough, new CompareSizeByArea());
-        } else if (notBigEnough.size() > 0) {
-            return Collections.max(notBigEnough, new CompareSizeByArea());
+        if (DeviceConfig.isInRokidGlass) {
+            return choices[5];
         } else {
-            Log.e(TAG, "Couldn't find any suitable preview size");
-            if (DeviceConfig.isInRokidGlass) {
-                return choices[5];
-            } else {
-                return choices[choices.length - 1];
-            }
+            return choices[choices.length - 1];
         }
     }
 
